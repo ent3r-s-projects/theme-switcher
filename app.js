5d1e6bacc40f692033161b43d3eec7eeeaf16b2f
ent3r <32072697+ent3r@users.noreply.github.com>
2020-03-30 19:56:40 +0200

    Add comments in script

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+//? Get the different buttons and the body
 var lightButton = document.getElementById("light");
 var darkButton = document.getElementById("dark");
 var solarButton = document.getElementById("solar");
 var body = document.body;
 
+//? Get the preferred theme from localStorage
 var theme = localStorage.getItem("theme");
 var isSolar = localStorage.getItem("solar");
 
+//? If the user has set a theme in their system settings, those settings are used
 if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
-  //? This means dark theme preference is set in the operating system settings
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     body.classList.replace("light", "dark");
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
@@ -15,6 +17,7 @@ if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
   }
 }
 
+//? Check if the user has set a theme, and apply it if they have
 if (theme) {
   if (theme === "dark") {
     body.classList.replace("light", theme);
@@ -23,21 +26,25 @@ if (theme) {
   }
 }
 
+//? Check if the user has solar theme on
 if (isSolar) {
   body.classList.add("solar");
   solarButton.innerText = "Not solarized";
 }
 
+//? Add the event listeners for the different buttons
 lightButton.addEventListener("click", () => {
   body.classList.replace("dark", "light");
   localStorage.setItem("theme", "light");
 });
 
+//? Add the event listeners for the different buttons
 darkButton.addEventListener("click", () => {
   body.classList.replace("light", "dark");
   localStorage.setItem("theme", "dark");
 });
 
+//? Add the event listeners for the different buttons
 solarButton.addEventListener("click", () => {
   let result = body.classList.toggle("solar");
   if (result) {
